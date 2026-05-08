9ecb9cf1773b6f5068c36e9264ee5e2a9537c4ee
Jakob Dunlap <jakob_dunlap@outlook.com>
2026-05-08 12:25:30 -0500

    First attempted integration of confirmation email management

--- server.js
+++ server.js
@@ -6,7 +6,11 @@ const url = process.env.DATABASE_URI;
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const cors = require('cors');
+const nodemailer = require('nodemailer');
 const PORT = process.env.PORT || 5000;
+
+const emailUser = process.env.EMAIL_USER;
+const emailPass = process.env.EMAIL_PASS;
 
 // Schema for guests who fill out the form
 const guestSchema = new Schema({
@@ -33,10 +37,19 @@ const guestSchema = new Schema({
     }
 });
 
+const transporter = nodemailer.createTransport({
+  host: 'smtp.gmail.com',
+  port: 465,
+  secure: true,
+  auth: {
+    user: emailUser,
+    pass: emailPass
+  }
+});
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 app.use('/', express.static('/public'));
 app.use('/', require('./root'));
 
@@ -44,7 +57,6 @@ app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
 
-//mongoose.connect(process.env.DATABASE_URI)
 mongoose.connect(url)
   .then(() => console.log("Successfully connected to MongoDB"))
   .catch(err => {
@@ -52,15 +64,27 @@ mongoose.connect(url)
     process.exit(1);
   });
 
-
-
 app.post("/form", async (req, res) => {
   const formData = req.body;
+  const emailAddressFromForm = formData.email;
   const Guest = mongoose.model("Guest", guestSchema);
   const newItem = new Guest(formData);
   try {
     await newItem.save();
     res.status(201).json({ message: "Data saved successfully" });
+
+    // Send email
+    console.log(`Attempting to send confirmation email to "${emailAddressFromForm}"`);
+    transporter.sendMail({
+      to: emailAddressFromForm,
+      subject: 'ya got mail',
+      html: '<h1>Hi how are you</h1><br><p>subtext</p>'
+    }).then(() => {
+      console.log('Email sent');
+    }).catch(err => {
+      console.error(err);
+    });
+
   } catch (err) {
     console.error(err);
     res.status(400).send("Unable to save to database");
