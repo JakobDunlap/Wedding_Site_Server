0ea87f2934d2c0601dbb910a6f7879b5fa1813bf
Jakob Dunlap <jakob_dunlap@outlook.com>
2026-05-13 11:05:48 -0500

    Debugging gmail auth

--- server.js
+++ server.js
@@ -39,8 +39,8 @@ const guestSchema = new Schema({
 
 const transporter = nodemailer.createTransport({
   host: 'smtp.gmail.com',
-  port: 465,
-  secure: true,
+  port: 587,
+  secure: false,
   auth: {
     user: emailUser,
     pass: emailPass
